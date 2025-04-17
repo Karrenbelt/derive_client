ab5dc546c4d3402612537a0bd59eba7b926c585f
zarathustra <zarathustra_git@pm.me>
2025-04-17 17:02:34 +0200

    refactor: add ApiException to `data_types/__init__.py`

--- derive_client/clients/__init__.py
+++ derive_client/clients/__init__.py
@@ -1,11 +1,12 @@
 """Clients module"""
 
 from .async_client import AsyncClient
-from .base_client import BaseClient
+from .base_client import BaseClient, ApiException
 from .http_client import HttpClient
 from .ws_client import WsClient
 
 __all__ = [
+    "ApiException",
     "BaseClient",
     "AsyncClient",
     "HttpClient",
