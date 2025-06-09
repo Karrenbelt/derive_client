ac775778af18b5412bd764f0d7291184cc3a04bd
zarathustra <zarathustra_git@pm.me>
2025-06-09 15:48:25 +0200

    feat: detect and download proxy contract ABIs, create proxy_mapping.json

--- derive_client/utils.py
+++ derive_client/utils.py
@@ -20,7 +20,16 @@ from web3.contract import Contract
 from web3.datastructures import AttributeDict
 
 from derive_client.constants import ABI_DATA_DIR, DATA_DIR
-from derive_client.data_types import ChainID, DeriveAddresses, MintableTokenData, RPCEndPoints, TxResult, TxStatus
+from derive_client.data_types import (
+    ChainID,
+    Currency,
+    DeriveAddresses,
+    MintableTokenData,
+    NonMintableTokenData,
+    RPCEndPoints,
+    TxResult,
+    TxStatus,
+)
 
 
 def get_logger():
@@ -86,7 +95,9 @@ def get_erc20_contract(w3: Web3, token_address: str) -> Contract:
     return get_contract(w3=w3, address=token_address, abi=abi)
 
 
-def wait_for_tx_receipt(w3: Web3, tx_hash: str, timeout=120, poll_interval=1) -> AttributeDict:
+def wait_for_tx_receipt(
+    w3: Web3, tx_hash: str, timeout=120, poll_interval=1
+) -> AttributeDict:
     start_time = time.time()
     while True:
         try:
@@ -167,14 +178,20 @@ def estimate_fees(w3, percentiles: list[int], blocks=20, default_tip=10_000):
     fee_estimations = []
     for priority_fee in avg_priority_fees:
         max_fee = latest_base_fee + priority_fee
-        fee_estimations.append({"maxFeePerGas": max_fee, "maxPriorityFeePerGas": priority_fee})
+        fee_estimations.append(
+            {"maxFeePerGas": max_fee, "maxPriorityFeePerGas": priority_fee}
+        )
 
     return fee_estimations
 
 
-def exp_backoff_retry(func=None, *, attempts=3, initial_delay=1, exceptions=(Exception,)):
+def exp_backoff_retry(
+    func=None, *, attempts=3, initial_delay=1, exceptions=(Exception,)
+):
     if func is None:
-        return lambda f: exp_backoff_retry(f, attempts=attempts, initial_delay=initial_delay, exceptions=exceptions)
+        return lambda f: exp_backoff_retry(
+            f, attempts=attempts, initial_delay=initial_delay, exceptions=exceptions
+        )
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -232,6 +249,18 @@ def get_retry_session(
     return session
 
 
+EIP1967_SLOT = (
+    int.from_bytes(Web3.keccak(text="eip1967.proxy.implementation")[:32], "big") - 1
+).to_bytes(32, "big")
+
+CHAIN_ID_TO_NETWORK_ID = {
+    ChainID.ETH: "ethereum",
+    ChainID.OPTIMISM: "optimism",
+    ChainID.ARBITRUM: "arbitrum",
+    ChainID.BASE: "base",
+}
+
+
 def get_abi(network_id, contract_address):
     ABI_DATA_URL = "https://abidata.net"
     url = f"{ABI_DATA_URL}/{contract_address}"
@@ -241,55 +270,78 @@ def get_abi(network_id, contract_address):
     session = get_retry_session()
     response = session.get(url, timeout=10)
     response.raise_for_status()
-    return response.json()
+    return response.json()["abi"]
+
+
+def collect_prod_addresses(
+    currencies: dict[Currency, NonMintableTokenData | MintableTokenData],
+):
+    contract_addresses = []
+    for currency, token_data in currencies.items():
+        if isinstance(token_data, MintableTokenData):
+            contract_addresses.append(token_data.Controller)
+            contract_addresses.append(token_data.MintableToken)
+        else:  # NonMintableTokenData
+            contract_addresses.append(token_data.Vault)
+            contract_addresses.append(token_data.NonMintableToken)
+
+        if token_data.LyraTSADepositHook is not None:
+            contract_addresses.append(token_data.LyraTSADepositHook)
+        if token_data.LyraTSAShareHandlerDepositHook is not None:
+            contract_addresses.append(token_data.LyraTSAShareHandlerDepositHook)
+        for connector_chain_id, connectors in token_data.connectors.items():
+            contract_addresses.append(connectors["FAST"])
+    return contract_addresses
+
+
+def get_impl_address(w3: Web3, address: str) -> str | None:
+    data = w3.eth.get_storage_at(address, EIP1967_SLOT)
+    impl_address = Web3.to_checksum_address(data[-20:])
+    if int(impl_address, 16) == 0:  # not a proxy
+        return
+    return impl_address
 
 
 if __name__ == "__main__":
-    chain_id_to_network_id = {
-        ChainID.ETH: "ethereum",
-        ChainID.OPTIMISM: "optimism",
-        ChainID.ARBITRUM: "arbitrum",
-        ChainID.BASE: "base",
-    }
-
+    logger = get_logger()
     prod_addresses = get_prod_derive_addresses()
 
     chain_addresses = {}
     for chain_id, currencies in prod_addresses.chains.items():
-        contract_addresses = []
-        for currency, token_data in currencies.items():
-            if isinstance(token_data, MintableTokenData):
-                contract_addresses.append(token_data.Controller)
-                contract_addresses.append(token_data.MintableToken)
-            else:  # NonMintableTokenData
-                contract_addresses.append(token_data.Vault)
-                contract_addresses.append(token_data.NonMintableToken)
-
-            if token_data.LyraTSADepositHook is not None:
-                contract_addresses.append(token_data.LyraTSADepositHook)
-            if token_data.LyraTSAShareHandlerDepositHook is not None:
-                contract_addresses.append(token_data.LyraTSAShareHandlerDepositHook)
-            for connector_chain_id, connectors in token_data.connectors.items():
-                contract_addresses.append(connectors["FAST"])
-        chain_addresses[chain_id] = contract_addresses
+        chain_addresses[chain_id] = collect_prod_addresses(currencies)
 
     failures = []
     abi_path = ABI_DATA_DIR.parent / "abis"
+
     for chain_id, addresses in chain_addresses.items():
-        if (network_id := chain_id_to_network_id.get(chain_id)) is None:
-            print(f"Network not supported by abidata.net: {chain_id.name}")
+        proxy_mapping = {}
+        w3 = get_w3_connection(chain_id=chain_id)
+
+        if (network_id := CHAIN_ID_TO_NETWORK_ID.get(chain_id)) is None:
+            logger.info(f"Network not supported by abidata.net: {chain_id.name}")
             continue
-        for address in addresses:
+
+        while addresses:
+            address = addresses.pop()
+            if impl_address := get_impl_address(w3=w3, address=address):
+                logger.info(
+                    f"EIP1967 Proxy implementation found: {address} -> {impl_address}"
+                )
+                addresses.append(impl_address)
+                proxy_mapping[address] = impl_address
             try:
                 abi = get_abi(network_id=network_id, contract_address=address)
             except Exception as e:
                 failures.append(f"{network_id}: {address}: {e}")
                 continue
+
             contract_abi_path = abi_path / network_id / f"{address}.json"
             contract_abi_path.parent.mkdir(exist_ok=True, parents=True)
             contract_abi_path.write_text(json.dumps(abi, indent=4))
 
+        proxy_mapping_path = abi_path / network_id / "proxy_mapping.json"
+        proxy_mapping_path.write_text(json.dumps(proxy_mapping, indent=4))
+
     if failures:
-        logger = get_logger()
         unattained = "\n".join(failures)
         logger.error(f"Failed to fetch:\n{unattained}")
